85e73955c04933ec509af0a864ed0ba58e766648
Layth Massoud <LaythMassoud@Layths-MacBook-Air.local>
2019-09-18 19:37:15 +0400

    changes to auth.js

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const keys = require('./config/keys');
-const passport = require('passport')
+const bodyParser = require('body-parser');
+const keys = require('./config/keys')
+const passport = require('passport');
 
 const db = keys.mongoURI;
 
 mongoose
-    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then( ()=> console.log("DB Connected"))
-    .catch(err=> console.log(err));
+    .connect(db, {useNewUrlParser:true, useUnifiedTopology: true })
+    .then(() => console.log("DB Is connected"))
+    .catch(err => console.log(err))
 
-
-const app = express();
+const app = express()
 app.use(bodyParser.urlencoded({extended: false}));
 
 //Init passport
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
-//Configure body-parser
-app.use(bodyParser.urlencoded({extended: false}));
+//Auth routes
+const authRoutes = require('./routes/Auth')
+app.use('/auth', authRoutes)
 
-// User routes
+//User routes
 const userRoutes = require('./routes/User');
-app.use('/users', userRoutes);
+app.use('/users', passport.authenticate('jwt', {session: false}), userRoutes);
 
 //Post routes
 const postRoutes = require('./routes/Post');
-app.use('/posts',passport.authenticate('jwt',{session: false}), postRoutes);
+app.use('/posts', passport.authenticate('jwt', {session: false}), postRoutes);
 
-//Homepage
-app.get('/', (req,res) => res.json({
-	msg: "Hello! What's Up, Amingo?"
-}));
+const port = process.env.PORT || 5000
+app.get('/', (req, res) => res.json({messsage: "Hello, what's up, Amigo!!˜"}))
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Your application is running @ http://localhost:${port}`));
+app.listen(port, () => console.log(`Your application is running @ http://localhost:${port}`))
